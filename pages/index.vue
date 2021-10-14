4320c22d493b8246c3ed59f5d16f4c7ea214ed8e
productfrontenddeveloper <38668796+productfrontenddeveloper@users.noreply.github.com>
2021-10-14 22:24:50 +0300

    fix: pages index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="text-white font-semibold p-4">
-    test
-  </div>
+    <div class="text-white font-semibold p-4 flex flex-col">
+        Tailwindcss Starter
+        <a
+            target="blank"
+            class="text-green-500"
+            href="https://github.com/productfrontenddeveloper/"
+        >Follow Me Github</a>
+    </div>
 </template>
